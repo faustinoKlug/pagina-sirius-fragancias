4f154569a1058d270c85c0ffc35268b53a444628
faustinoKlug <destroya2.xd@gmail.com>
2025-01-14 12:38:55 -0300

    lista de perfumes actualizada

--- src/imprimir_productos.js
+++ src/imprimir_productos.js
@@ -8,8 +8,24 @@ document.addEventListener("DOMContentLoaded", () => {
     
     perfumes_hombre.forEach(perfume => {
         const $div = document.createElement("div");
-        $div.classList.add("max-w-[270px]", "max-h-[320px]", "w-full", "my-8", "flex", "flex-col", "justify-between", "h-full");
-        $div.innerHTML = `
+        $div.classList.add("max-w-[270px]", "max-h-[320px]", "w-full", "my-8", "flex", "flex-col", "justify-between", "h-full", "relative");
+        if (perfume.stock === false) {
+            $div.innerHTML = `
+                <p class="absolute top-[10px] right-0 text-sm py-2 px-3 border-[1px] font-semibold border-white ">SIN STOCK</p>
+                <div class="w-full h-full flex flex-col items-center">
+                    <img src="${perfume.imagen}" alt="" class="w-[200px] h-[80%] object-cover cursor-pointer scale-hover">
+                    <p class="text-center font-bold uppercase mt-2 text-lg cursor-pointer">${perfume.nombre}</p>
+                    <p class="text-center">$${perfume.precio}</p>
+                </div>
+                <div class="mt-2">
+                    <button class="w-full border-[1px] cursor-default p-1 flex justify-center items-center gap-x-1" data-id="${perfume.id}">
+                    AÑADIR
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#ffffff" d="M12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q1.35 0 2.6-.437t2.3-1.263L5.7 7.1q-.825 1.05-1.263 2.3T4 12q0 3.35 2.325 5.675T12 20m6.3-3.1q.825-1.05 1.263-2.3T20 12q0-3.35-2.325-5.675T12 4q-1.35 0-2.6.437T7.1 5.7z"/></svg>
+                    </button>
+                </div>
+            `;
+        } else {
+            $div.innerHTML = `
             <div class="w-full h-full flex flex-col items-center">
                 <img src="${perfume.imagen}" alt="" class="w-[200px] h-[80%] object-cover cursor-pointer scale-hover">
                 <p class="text-center font-bold uppercase mt-2 text-lg cursor-pointer">${perfume.nombre}</p>
@@ -19,13 +35,30 @@ document.addEventListener("DOMContentLoaded", () => {
                 <button class="w-full border-[1px] btn-carrito p-1 btn-agregar" data-id="${perfume.id}">AÑADIR</button>
             </div>
         `;
+        }
         $productsContainerM.appendChild($div);
     });
 
     perfumes_mujer.forEach(perfume => {
         const $div = document.createElement("div");
-        $div.classList.add("max-w-[270px]", "max-h-[320px]", "w-full", "my-8", "flex", "flex-col", "justify-between", "h-full");
-        $div.innerHTML = `
+        $div.classList.add("max-w-[270px]", "max-h-[320px]", "w-full", "my-8", "flex", "flex-col", "justify-between", "h-full", "relative");
+        if (perfume.stock === false) {
+            $div.innerHTML = `
+                <p class="absolute top-[10px] right-0 text-sm py-2 px-3 border-[1px] font-semibold border-white ">SIN STOCK</p>
+                <div class="w-full h-full flex flex-col items-center">
+                    <img src="${perfume.imagen}" alt="" class="w-[200px] h-[80%] object-cover cursor-pointer scale-hover">
+                    <p class="text-center font-bold uppercase mt-2 text-lg cursor-pointer">${perfume.nombre}</p>
+                    <p class="text-center">$${perfume.precio}</p>
+                </div>
+                <div class="mt-2">
+                    <button class="w-full border-[1px] cursor-default p-1 flex justify-center items-center gap-x-1" data-id="${perfume.id}">
+                    AÑADIR
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#ffffff" d="M12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q1.35 0 2.6-.437t2.3-1.263L5.7 7.1q-.825 1.05-1.263 2.3T4 12q0 3.35 2.325 5.675T12 20m6.3-3.1q.825-1.05 1.263-2.3T20 12q0-3.35-2.325-5.675T12 4q-1.35 0-2.6.437T7.1 5.7z"/></svg>
+                    </button>
+                </div>
+            `;
+        } else {
+            $div.innerHTML = `
             <div class="w-full h-full flex flex-col items-center">
                 <img src="${perfume.imagen}" alt="" class="w-[200px] h-[80%] object-cover cursor-pointer scale-hover">
                 <p class="text-center font-bold uppercase mt-2 text-lg cursor-pointer">${perfume.nombre}</p>
@@ -35,6 +68,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 <button class="w-full border-[1px] btn-carrito p-1 btn-agregar" data-id="${perfume.id}">AÑADIR</button>
             </div>
         `;
+        }
+
         $productsContainerF.appendChild($div);
     });
 });
--- src/lista_productos.js
+++ src/lista_productos.js
@@ -5,7 +5,8 @@ const perfumes_hombre = [
         precio: 26999.00,
         cantidad: 1,
         descripcion: "Una fragancia misteriosa y seductora, perfecta para el hombre nocturno que busca dejar una impresión duradera.",
-        imagen: "assets/kenzo-night.png"
+        imagen: "assets/kenzo-night.png",
+        stock: true
     },
     {
         id: 2,
@@ -13,7 +14,8 @@ const perfumes_hombre = [
         precio: 28999.00,
         cantidad: 1,
         descripcion: "Una fragancia sofisticada y energética, pensada para hombres que buscan destacar en cualquier ocasión exclusiva.",
-        imagen: "assets/212-varon.png"
+        imagen: "assets/212-varon.png",
+        stock: true
     },
     {
         id: 3,
@@ -21,7 +23,8 @@ const perfumes_hombre = [
         precio: 27999.00,
         cantidad: 1,
         descripcion: "Una fragancia elegante y fresca, que refleja el poder y la confianza del hombre moderno.",
-        imagen: "assets/versacce.png"
+        imagen: "assets/versacce.png",
+        stock: true
     },
     {
         id: 4,
@@ -29,7 +32,8 @@ const perfumes_hombre = [
         precio: 26999.00,
         cantidad: 1,
         descripcion: "Una fragancia fresca y ligera, perfecta para hombres que buscan una esencia versátil y de larga duración.",
-        imagen: "assets/polo-blue.png"
+        imagen: "assets/polo-blue.png",
+        stock: true
     },
     {
         id: 5,
@@ -37,7 +41,8 @@ const perfumes_hombre = [
         precio: 26999.00,
         cantidad: 1,
         descripcion: "Una fragancia rica y especiada, que evoca la opulencia y el poder, diseñada para hombres que desean ser recordados.",
-        imagen: "assets/one-million.png"
+        imagen: "assets/one-million.png",
+        stock: true
     },
     {
         id: 6,
@@ -45,7 +50,8 @@ const perfumes_hombre = [
         precio: 27999.00,
         cantidad: 1,
         descripcion: "Una fragancia vibrante y poderosa, pensada para hombres que buscan transmitir fuerza y victoria en cada paso.",
-        imagen: "assets/invictus.png"
+        imagen: "assets/invictus.png",
+        stock: true
     },
     {
         id: 7,
@@ -53,58 +59,110 @@ const perfumes_hombre = [
         precio: 26999.00,
         cantidad: 1,
         descripcion: "Una fragancia vibrante y poderosa, pensada para hombres que buscan transmitir fuerza y victoria en cada paso.",
-        imagen: "assets/dolce-varon.png"
-    }
+        imagen: "assets/dolce-varon.png",
+        stock: true
+    },
+    {
+        id: 8,
+        nombre: `ANGE OU DEMON `,
+        precio: 24599.00,
+        cantidad: 1,
+        descripcion: "",
+        imagen: "assets/ange-ou-demon-hombre.png",
+        stock: false
+    },
+    {
+        id: 9,
+        nombre: `ACQUA DI GIO`,
+        precio: 25600.00,
+        cantidad: 1,
+        descripcion: "",
+        imagen: "assets/acqua.png",
+        stock: false
+    },
 ];
 
 const perfumes_mujer = [
     {
-        id: 8,
+        id: 10,
         nombre: `CH HC MUJER ROJO`,
         precio: 26900.00,
         cantidad: 1,
         descripcion: "Una fragancia ardiente y cautivadora, diseñada para mujeres apasionadas y con una personalidad vibrante.",
-        imagen: "assets/ch-hc.png"
+        imagen: "assets/ch-hc.png",
+        stock: true,
     },
     {
-        id: 9,
+        id: 11,
         nombre: `212 SEXY MUJER`,
         precio: 25999.00,
         cantidad: 1,
         descripcion: "Una fragancia femenina y sensual, ideal para mujeres que buscan expresar su lado más atrevido y encantador.",
-        imagen: "assets/212-sexy.png"
+        imagen: "assets/212-sexy.png",
+        stock: true,
     },
     {
-        id: 10,
-        nombre: `ANGE OU DEMON MUJER`,
+        id: 12,
+        nombre: `ANGE OU DEMON IN PINK`,
         precio: 25999.00,
         cantidad: 1,
         descripcion: "Una fragancia intrigante, mezcla de dulzura y misterio, para mujeres seductoras que juegan con su dualidad.",
-        imagen: "assets/ance-givenchy.png"
+        imagen: "assets/ance-givenchy.png",
+        stock: false
     },
     {
-        id: 11,
+        id: 13,
         nombre: `KENZO KISS`,
         precio: 24999.00,
         cantidad: 1,
         descripcion: "Una fragancia floral y afrutada, fresca y juvenil, ideal para mujeres que desean transmitir una sensación de dulzura y libertad.",
-        imagen: "assets/kizz.png"
+        imagen: "assets/kizz.png",
+        stock: true,
     },
     {
-        id: 12,
+        id: 14,
         nombre: `FAME`,
         precio: 29999.00,
         cantidad: 1,
         descripcion: "Una fragancia audaz y empoderante, con notas florales y orientales, diseñada para mujeres que se atreven a destacar.",
-        imagen: "assets/fame.png"
+        imagen: "assets/fame.png",
+        stock: true,
     },
     {
-        id: 13,
+        id: 15,
         nombre: `GOOD GIRL`,
         precio: 28900.00,
         cantidad: 1,
         descripcion: "Una fragancia sofisticada y atrevida, que mezcla lo dulce y lo oscuro para una mujer que es a la vez angelical y rebelde.",
-        imagen: "assets/carolina-good-girl.png"
+        imagen: "assets/carolina-good-girl.png",
+        stock: true,
+    },
+    {
+        id: 16,
+        nombre: `FLOWERBY`,
+        precio: 28900.00,
+        cantidad: 1,
+        descripcion: "",
+        imagen: "assets/flower-b.png",
+        stock: false,
+    },
+    {
+        id: 17,
+        nombre: `MADAME - 50ml`,
+        precio: 24599.00,
+        cantidad: 1,
+        descripcion: "",
+        imagen: "assets/madame.png",
+        stock: false,
+    },
+    {
+        id: 18,
+        nombre: `212 ROSÉ - 50ml`,
+        precio: 28499.00,
+        cantidad: 1,
+        descripcion: "",
+        imagen: "assets/212-rose.png",
+        stock: false,
     },
 ]
 
